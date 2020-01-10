cf096e4aff32f9469ddbb1f86920a374dc3669b5
Leanna Hue <leanna.hue@gmail.com>
2020-01-10 10:24:42 -0600

    small change, added a class name

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () =>  (
   <div>
     <h1>This is our OfficeHours Pro</h1>
     <h3>CS 394: Agile Software Development</h3>
+    <h3>CS 340: Introduction to Computer Networking</h3>
   </div>
 );
 
