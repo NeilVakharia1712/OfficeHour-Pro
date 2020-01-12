64f2bc1951acebe81445dc2222f3532b717d458c
Neil Vakharia <NeilVakharia2021@u.northwestern.edu>
2020-01-12 17:51:34 -0600

    CheckIn FireBase

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -5,6 +5,8 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import 'firebase/database';
+import firebase from 'firebase/app';
 
 const useStyles = makeStyles({
   card: {
@@ -23,6 +25,24 @@ const useStyles = makeStyles({
   },
 });
 
+const CheckIn = (props) => {
+
+  const db = firebase.database();
+  console.log(props.user.uid);
+  console.log(props.courseName); 
+  const CheckedInUser = firebase.database().ref('courses/' + props.courseNumber + '/officeHours');
+  CheckedInUser.on('value', function(snapshot){
+    console.log(snapshot.val());
+  });
+  console.log(CheckedInUser); 
+
+  firebase.database().ref('courses/' + props.courseNumber + '/officeHours').update({
+    [props.user.uid]: props.user.uid
+  })
+
+  
+}
+
 const CourseCard = props => {
   const classes = useStyles();
 
@@ -38,10 +58,11 @@ const CourseCard = props => {
         <Typography variant="body2" component="p">
           Incomming office hour: Jan 28 7:00 P.M.
         </Typography>
+
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
-        <Button color="secondary" size="small">Check in</Button>
+        <Button onClick={()=>{CheckIn(props)}} color="secondary" size="small">Check in</Button>
       </CardActions>
     </Card>
   );
