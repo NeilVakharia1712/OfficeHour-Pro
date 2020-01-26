ea3c1fed995b899ee717c59ddf4387fa709fcd6f
Leanna Hue <leanna.hue@gmail.com>
2020-01-26 16:31:41 -0600

    changed column padding

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -24,7 +24,7 @@ const CourseList = ({
     const length = courses.length;
     return (
         <Column.Group style={{ marginTop: "5px", background: "white" }}>
-          <Column style={{ paddingTop: "0px", paddingBottom: "0px" }}>
+          <Column style={{ paddingTop: "0px", paddingBottom: "0px", paddingLeft: "5px", paddingRight: "5px" }}>
             {courses.slice(0, Math.ceil(length / 2)).map(course => {
               return (
                 <CourseCard
@@ -38,7 +38,7 @@ const CourseList = ({
               );
             })}
           </Column>
-          <Column style={{ paddingTop: "0px", paddingBottom: "0px" }}>
+          <Column style={{ paddingTop: "0px", paddingBottom: "0px", paddingLeft: "5px", paddingRight: "5px" }}>
             {courses.slice(Math.ceil(length / 2), length).map(course => {
               return (
                 <CourseCard
