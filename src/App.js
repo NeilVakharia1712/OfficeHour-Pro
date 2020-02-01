fcbf27e3254f743f2078a3bb71fabcff5e09938c
ivyxuan <iloveivyxuan@gmail.com>
2020-02-01 16:57:54 -0600

    Check role of the user

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const App = () => {
 	const [schedule, setSchedule] = useState(null);
 	const [courses, setCourse] = useState([]);
 	const [checkedInCourse, setCheckedInCourse] = useState(null);
+	const [isProf, setIsProf] = useState(false);
 
 	useEffect(() => {
 		firebase.auth().onAuthStateChanged(setUser);
@@ -39,8 +40,14 @@ const App = () => {
 		courseDb.once("value", getCourseInfo, error => alert(error));
 	}, [])
 
+	const verifyEmail = (email) => {
+		const re = /u.northwestern.edu/g;
+		return re.test(email)
+	}
+
 	useEffect(() => {
 		if (user) {
+			setIsProf(verifyEmail(user.email))
 			const updateUserCourse = (snapshot) => {
 				if (snapshot.val()) {
 					setCourse(snapshot.val().courses)
@@ -53,7 +60,6 @@ const App = () => {
 			}
 			const userDb = firebase.database().ref('Users/' + user.uid);
 			userDb.on('value', updateUserCourse);
-			// eslint-disable-next-line
 		}
 	}, [user])
 
