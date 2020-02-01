f3b1dd92fdebff9a45754a08d6dbe67c1ab1ab53
Neil Vakharia <NeilVakharia2021@u.northwestern.edu>
2020-02-01 17:41:30 -0600

    Create Dialog Box

--- src/components/editOfficeHours.js
+++ src/components/editOfficeHours.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Button, Radio, FormControlLabel, RadioGroup } from '@material-ui/core';
+import { Button, Radio, FormControlLabel, RadioGroup, ListItem, List } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
@@ -36,34 +35,74 @@ const FormDialog = () => {
 				<DialogTitle id="alert-dialog-title">Edit Office Hours</DialogTitle>
 				<DialogContent>
 					<form noValidate autoComplete="off">
-						<TextField id="standard-basic" label="Instructor" />
-						<RadioGroup row aria-label="gender" name="gender2" value={weekday} onChange={handleChangeWeekday}>
-							<FormControlLabel
-								value="mo"
-								control={<Radio color="primary" />}
-								label="Mo"
-							/>
-							<FormControlLabel
-								value="tu"
-								control={<Radio color="primary" />}
-								label="Tu"
-							/>
-							<FormControlLabel
-								value="we"
-								control={<Radio color="primary" />}
-								label="We"
-							/>
-							<FormControlLabel
-								value="th"
-								control={<Radio color="primary" />}
-								label="Th"
-							/>
-							<FormControlLabel
-								value="fr"
-								control={<Radio color="primary" />}
-								label="Fr"
-							/>
-						</RadioGroup>
+						<List style = {{paddingTop: '0%'}}>
+							<ListItem>
+								<TextField id="standard-basic" label="Instructor" />
+							</ListItem>
+							<ListItem>
+								<TextField id="standard-basic" label="Instructor Email" />
+							</ListItem>
+							<ListItem>
+								<RadioGroup row aria-label="gender" name="gender2" value={weekday} onChange={handleChangeWeekday}>
+								<FormControlLabel
+									value="mo"
+									control={<Radio color="primary" />}
+									label="Mo"
+								/>
+								<FormControlLabel
+									value="tu"
+									control={<Radio color="primary" />}
+									label="Tu"
+								/>
+								<FormControlLabel
+									value="we"
+									control={<Radio color="primary" />}
+									label="We"
+								/>
+								<FormControlLabel
+									value="th"
+									control={<Radio color="primary" />}
+									label="Th"
+								/>
+								<FormControlLabel
+									value="fr"
+									control={<Radio color="primary" />}
+									label="Fr"
+								/>
+								</RadioGroup>
+							</ListItem>
+							<ListItem>
+								<TextField
+									id="time"
+									label="Start Time"
+									type="time"
+									defaultValue="07:30"
+									InputLabelProps={{
+									shrink: true,
+									}}
+									inputProps={{
+									step: 300, // 5 min
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<TextField
+									id="time"
+									label="End Time"
+									type="time"
+									defaultValue="07:30"
+									InputLabelProps={{
+									shrink: true,
+									}}
+									inputProps={{
+										step: 300, // 5 min
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<TextField id="standard-basic" label="Location" />
+							</ListItem>
+						</List>
 					</form>
 				</DialogContent>
 			</Dialog>
